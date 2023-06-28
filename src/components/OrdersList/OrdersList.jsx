c5825e2082e9d0d47b7c3a770e6ddd551d66d8ae
Tatyana Tupalo <tatyana080489t@gmail.com>
2023-06-28 19:06:12 +0300

    fix orderList

--- src/components/OrdersList/OrdersList.jsx
+++ src/components/OrdersList/OrdersList.jsx
@@ -51,14 +51,18 @@ const OrdersList = ({ orders }) => {
               <li>Передзвонити: {order.callBack === false ? "так" : "ні"}</li>
               <li>Коментар: {order.comment}</li>
             </ul>
-            <ul className={s.orders_list__item_userList_composition}>
-              <span>Композиція</span>
-              {order.basket.balloons.map((b) => (
-                <li key={b.id}>
-                  <Basket basket={b} />
-                </li>
-              ))}
-            </ul>
+            {order.basket.balloons && (
+              <ul className={s.orders_list__item_userList_composition}>
+                <span>Композиція</span>
+
+                {order.basket.balloons.map((b) => (
+                  <li key={b.id}>
+                    <Basket basket={b} />
+                  </li>
+                ))}
+              </ul>
+            )}
+
             <p className={s.orders_list_amount}>
               Сума заказу: {order.basket.amount} грн
             </p>
